1ce71be0f923f7b1f101daf5454d6c97be0d3513
hagonin <goninha@outlook.fr>
2025-11-24 14:40:45 +0100

    fix(config): setup server.js - remove unused logs in database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,18 +1,14 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      // Mongoose 6+ no longer needs these options:
-      // useNewUrlParser: true,
-      // useUnifiedTopology: true,
-    });
+	try {
+		const conn = await mongoose.connect(process.env.MONGO_URI, {});
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
-  }
+		console.log(`MongoDB Connected: ${conn.connection.host}`);
+	} catch (error) {
+		console.error(`Error: ${error.message}`);
+		process.exit(1);
+	}
 };
 
 module.exports = connectDB;
